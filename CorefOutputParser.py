fb9261d8e2406bd9cb98dc606bc42d093669f1de
jakob1 <j.wasserthal@gmx.de>
2015-07-08 18:19:54 +0200

    improved raw coref file parsing

--- CorefOutputParser.py
+++ CorefOutputParser.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import re
 import os, sys
+import nltk
 
 class CorefOutputParser:
     
@@ -10,67 +11,123 @@ class CorefOutputParser:
         self.df = pd.read_csv(file, sep='\t', header=None)
         self.df.columns = ["document_name","some id", "sentence_word_nr", "word", "pos_tag", "parsing_part", "", "", "", "", "", "coref"]
     
+    def __get_entity(self, coref_num, start_index):
+        
+        entity = ""
+        search_length = 5
+        good_tags = ["DT", "NNP", "NNS", "NN", "NNPS", "PRP", "PRP$", "JJ", "JJR", "JJS", "CC", "CD", "POS", "IN"]
+        important_tags = ["NNP", "NNS", "NN", "NNPS"]
+        found_end = False
+        end_index = None
+        contains_important_tag = False
+        
+        for index in range(start_index, start_index + search_length):
+                
+            if index >= self.df.shape[0]:
+                break
+                
+            if not isinstance(self.df.ix[index, "coref"], str):
+                continue #jump to next index
+            
+            entity += self.df.ix[index, "word"] + " "
+            
+            #queck for bad pos_tags in entity
+            if self.df.ix[index, "pos_tag"] not in good_tags:
+                return (None, None)
+            
+            if self.df.ix[index, "pos_tag"] in important_tags:
+                contains_important_tag = True
+                
+            #find end of entity
+            coref = self.df.ix[index, "coref"]
+            digits_in_coref = re.findall("\d+", coref)
+            if ")" in coref and str(coref_num) in digits_in_coref:
+                found_end = True
+                end_index = index
+                break
+                
+        if found_end and contains_important_tag:
+            return (entity.strip(), end_index)
+        else:
+            return (None, None)
+            
     def __create_entity_dict(self):
         '''
         private
         '''
-        current_entity = ""
-        in_entity = False
         current_coref_num = None
         
         for index, row in self.df.iterrows():
-            #print("!!!!----!!!!")
-            #print(row["coref"])
             if (isinstance(row["coref"], str)):
                 match = re.search("\d+", row["coref"])
                 if match != None:
                     current_coref_num = int(match.group())
                 
                 if "(" in row["coref"] and ")" in row["coref"]:
-                    current_entity = row["word"]
+                    tmp_entity = row["word"]
                     if current_coref_num not in self.entity_dict:
-                        self.entity_dict[current_coref_num] = current_entity
-                    current_entity = ""
-                    in_entity = False
+                        self.entity_dict[current_coref_num] = tmp_entity
                 elif "(" in row["coref"]:
-                    in_entity = True
-                    current_entity = row["word"] + " "
-                elif ")" in row["coref"]:
-                    current_entity += row["word"]
-                    if current_coref_num not in self.entity_dict:
-                        self.entity_dict[current_coref_num] = current_entity
-                    in_entity = False
-                    current_entity = ""
-                elif in_entity == True:
-                    current_entity += row["word"] + " "
+                    tmp_entity, end_index = self.__get_entity(current_coref_num, index)
+                    if tmp_entity != None and current_coref_num not in self.entity_dict:
+                        self.entity_dict[current_coref_num] = tmp_entity
     
     def get_resolved_text(self):
         self.__create_entity_dict()
         
         self.count = 0
-        text = ""
-        in_entity = False
+        self.count_removed_sents = 0
+        total_text = ""
+        sent_text = ""
         coref_num = None
-        for index, row in self.df.iterrows():
+        sent_contains_entity = False
+        
+        length = self.df.shape[0]
+        index = 0
 
+        while index < length:
+            row = self.df.loc[index]
+            found_entity = False
             if (isinstance(row["coref"], str)):
+                
+                if row["sentence_word_nr"] == 0:
+                    if sent_contains_entity:
+                        total_text += sent_text
+                    else:
+                        self.count_removed_sents += 1
+                        
+                    sent_text = ""
+                    sent_contains_entity = False
+                        
+                    
                 match = re.search("\d+", row["coref"])
                 if match != None:
                     coref_num = int(match.group())
+                    sent_contains_entity = True
                     
-                if "(" in row["coref"] and ")" in row["coref"]:
-                    text += self.entity_dict[coref_num] + " "
+                if "(" in row["coref"] and ")" in row["coref"] and coref_num in self.entity_dict:
+                    sent_text += self.entity_dict[coref_num] + " "
                     self.count += 1
-                elif "(" in row["coref"]:
-                    in_entity = True
-                elif ")" in row["coref"]:
-                    text += self.entity_dict[coref_num] + " "
-                    self.count += 1
-                    in_entity = False
-                elif in_entity == False:
-                    text += row["word"] + " "
+                    found_entity = True
+                elif "(" in row["coref"] and coref_num in self.entity_dict:
+                    tmp_entity, end_index = self.__get_entity(coref_num, index)
+                    if tmp_entity:
+                        sent_text += self.entity_dict[coref_num] + " "
+                        index = end_index
+                        self.count += 1
+                        found_entity = True
+                        
+                if not found_entity:
+                    sent_text += row["word"] + " "
+                    
+                    
+            index += 1
             
-        return text
+        total_text = "\n".join(nltk.sent_tokenize(total_text))
+        total_text = total_text.replace("-RRB-", ")")
+        total_text = total_text.replace("-LRB-", "(")
+        
+        return total_text
     
     def get_orig_text(self):
         text = ""
--- coref_runner.py
+++ coref_runner.py
@@ -1,52 +1,72 @@
 import os
 from CorefOutputParser import CorefOutputParser
+import sys, getopt
 
 if __name__ == "__main__":
+    
+    skipCorefBasics = False 
+    try:
+       opts, args = getopt.getopt(sys.argv[1:],"hs")
+    except getopt.GetoptError:
+       print('coref_runner.py [-s] \n s: skip berkeley coref resolution und starting with the already processed files')
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print('coref_runner.py [-s] \n s: skip berkeley coref resolution und starting with the already processed files')
+          sys.exit()
+       elif opt == "-s":
+          skipCorefBasics = True
 
     input_file_name = ""
 
-    print("--preprocessing....")
-    os.system("java -cp berkeleycoref-1.1.jar -Xmx10g edu.berkeley.nlp.coref.preprocess.PreprocessingDriver ++base.conf \
-                -execDir tmp \
-                -inputDir input \
-                -outputDir output_preprocessing")
-
-    print("--changing file endings...")
-    dir = "./output_preprocessing"
+    #get filenames
+    dir = "./input"
     fnames = os.listdir(dir)
     for fname in fnames:        
         name = fname.split(".")[0]
         input_file_name = name
-        print(fname)
-        if name != "":
-            os.rename(os.path.join(dir, fname), os.path.join(dir, name + ".auto_conll"))
 
-    print("--extracting coreferences...")
-    os.system("java -jar -Xmx10g berkeleycoref-1.1.jar ++base.conf \
-                  -execDir tmp \
-                  -modelPath models/coref-rawtext-final.ser \
-                  -testPath output_preprocessing \
-                  -outputPath output_coref/berkeley_output.txt \
-                  -mode PREDICT")
-    
-    print("--preparing output file (remove first and last line) ...")
-    readFile = open("./output_coref/berkeley_output.txt")
-    lines = readFile.readlines()
-    readFile.close()
-    w = open("tmp/berkeley_output_tmp.txt",'w')
-    w.writelines([item for item in lines[1:-1]])
-    w.close()
+    if not skipCorefBasics:
+
+        print("--preprocessing....")
+        os.system("java -cp berkeleycoref-1.1.jar -Xmx10g edu.berkeley.nlp.coref.preprocess.PreprocessingDriver ++base.conf \
+                    -execDir tmp \
+                    -inputDir input \
+                    -outputDir output_preprocessing")
+
+        print("--changing file endings...")
+        dir = "./output_preprocessing"
+        fnames = os.listdir(dir)
+        for fname in fnames:        
+            name = fname.split(".")[0]
+            input_file_name = name
+            print(fname)
+            if name != "":
+                os.rename(os.path.join(dir, fname), os.path.join(dir, name + ".auto_conll"))
+
+        print("--extracting coreferences...")
+        os.system("java -jar -Xmx10g berkeleycoref-1.1.jar ++base.conf \
+                      -execDir tmp \
+                      -modelPath models/coref-rawtext-final.ser \
+                      -testPath output_preprocessing \
+                      -outputPath tmp/berkeley_output_tmp.txt \
+                      -mode PREDICT")
+
+        print("--preparing output file (remove first and last line) ...")
+        readFile = open("tmp/berkeley_output_tmp.txt")
+        lines = readFile.readlines()
+        readFile.close()
+        w = open("output_coref/" + input_file_name + "-coref-raw.txt",'w')
+        w.writelines([item for item in lines[1:-1]])
+        w.close()
+
 
     print("--generating textfile with resolutions...")
-    corefOutputParser = CorefOutputParser("tmp/berkeley_output_tmp.txt")
-    
-    #corefOutputParser.print_df()
+    corefOutputParser = CorefOutputParser("output_coref/" + input_file_name + "-coref-raw.txt")
 
     new_text = corefOutputParser.get_resolved_text()
     print("-- number of resolved referenes: ", corefOutputParser.count)
-
-    #corefOutputParser.create_entity_dict()
-    #corefOutputParser.entity_dict
+    print("-- number of removed sents (lacking entity): ", corefOutputParser.count_removed_sents)
 
     w = open("output_coref/" + input_file_name + "-resolved.txt",'w')
     w.write(new_text)
@@ -59,8 +79,8 @@ if __name__ == "__main__":
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
-        except Exception, e:
-            print e
+        except Exception as e:
+            print(e)
 
     print("SUCCESSFULLY FINISHED")
 
